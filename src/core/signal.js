550c8e92818aa186a7772933ae2956ddb3b880d4
Jashepp <unchosen.aus@gmail.com>
2026-04-15 16:12:52 +0800

    Signal file split

---
+++ src/core/signal.js
@@ -0,0 +1,7 @@
+
+import { signalController } from "./signal/controller.js";
+import { signalObserver } from "./signal/observer.js";
+import { signalInstance, signalSymb } from "./signal/instance.js";
+import { signalProxy, resolveSignal } from "./signal/proxy.js";
+
+export { signalController, signalObserver, signalProxy, resolveSignal, signalInstance, signalSymb };
